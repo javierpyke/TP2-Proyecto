23d732f21f036cd9a18185b840ea857fd493bee2
JavierToso <ortjavier1995@gmail.com>
2022-05-24 21:53:17 -0300

    Generación de productos con Falso
    Seed de productos con Falso

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const puerto = 8080
 const server = app.listen(puerto)
 
 server.on('listening', () => { console.log('conectado!') })
 server.on('error', error => { console.log(error.message) })
+
+const {Product} = require('./src/db/models')
+
+app.get('/products', async (req,res) => {
+  let data = await Product.findAll()
+
+  res.send(data)
+})
+
+app.get('/products/:id', async (req,res) => {
+  let data = await Product.findByPk(req.params.id);
+
+  res.send(data)
+})
+
+app.get('/products-create', async (req,res) => {
+  await Product.create({
+    productName: "Rexona MaxSports",
+    description: "Desodorante Rexona 300ml - Duracion prolongada",
+    category: 1
+  })
+  await Product.create({
+    productName: "Rexona MasBlancos",
+    description: "Desodorante Rexona 350ml - Blancos mas blancos",
+    category: 1
+  })
+
+  res.send('Created')
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!!')
---
+++ src/db/seeders/20220518011506-products.js
@@ -0,0 +1,27 @@
+'use strict';
+const {randProductName , randProductDescription, rand} = require('@ngneat/falso');
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    let products = [];
+
+    for(let i = 0; i < 100; i++){
+      products.push({
+        productName: randProductName(),
+        description: randProductDescription(),
+        category: rand([ 1, 2, 3 ]),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })
+    }
+
+    await queryInterface.bulkInsert('Products', products, {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Products', null, {});
+  }
+};
